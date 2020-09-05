6fc1d9c21fef509aa1127b8903ec4e0fa3ee22ff
AzureAD\JustinHare <justin.hare@kalypso.com>
2020-09-05 14:39:30 -0400

    Updated complimentary string creation

--- PCR.py
+++ PCR.py
@@ -1,4 +1,16 @@
-with open("genome.txt") as file:
-    data = file.read()
+from csv import reader
+import numpy as np
+import pandas as pd
 
-print(data)
+with open('genome.txt', 'r') as file:
+    genome = file.read()
+
+genome = pd.DataFrame(list(reader(genome)))
+
+conditions = [genome[0] == 'A', genome[0] == 'T', genome[0] == 'G', genome[0] == 'C']
+
+choices = ['T', 'A', 'C', 'G']
+
+genome[1] = np.select(conditions, choices)
+
+print(genome.head())
